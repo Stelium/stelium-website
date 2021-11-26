56611291ed394bb74ae50e409322ba4a61760474
yasirbegig <yasir@begig.io>
2021-11-26 23:41:39 +0530

    ComingSoon

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Loaded')
+
+var userEmail = document.getElementById("user-email")
+var formSubmit = document.getElementById("form-submit")
+
+// userEmail.onchange = (e)=>{
+//     console.log(e.target.value)
+// }
+
+// formSubmit.onclick = ()=>{
+//     console.log(userEmail)
+// }
